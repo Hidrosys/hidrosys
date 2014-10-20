d087291a94f5c73ec01dd73fc946b928a8a691ca
renansd <renansouzadaniel@yahoo.com.br>
2014-10-19 22:31:10 -0200

    Template cabeçalho das páginas
    -Template para o cabeçalho das páginas;
    -Modificação no index.php para utilizar o novo template.

---
+++ templatepag.php
@@ -0,0 +1,47 @@
+<?php
+function template()
+{
+	if(verificaSessao() == false)
+    {
+      header("Location: login.php");
+    }
+    	
+	echo '<div class="panel panel-default">
+      <div class="panel-body">
+        <div class="row">
+
+          <div class="col-xs-2"><h3>HIDROSYS</h3></div>
+
+          <div class="col-xs-8" style="padding-top:12px">
+            <ul class="nav nav-pills">
+              <li class="active"><a href="#">Principal</a></li>
+              <li><a href="#">Clientes</a></li>
+              <li><a href="#">Serviços</a></li>
+              <li><a href="#">Funcionários</a></li>
+              <li><a href="#">Ferramentas</a></li>
+              <li><a href="#">Peças</a></li>
+              <li><a href="#">Relatórios</a></li>
+            </ul>
+          </div>
+
+          <div class="col-xs-2" style="padding-top:12px">
+            <ul class="nav nav-pills">
+              <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                  <?php echo getLogin(); ?> <span class="caret"></span>
+                </a>
+
+                <ul class="dropdown-menu" role="menu">
+                  <li class="item">
+                    <a href="login.php?logoff=true">Logoff</a>
+                  </li>
+                </ul>
+              </li>      
+            </ul>      
+          </div>
+
+        </div>
+      </div>
+    </div>';
+}
+?>
