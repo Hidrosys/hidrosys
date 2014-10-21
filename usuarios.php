a0ec6c186bf487b75aa4244cec1b6887153a9c89
thiagocarvp <thiago.carvp@gmail.com>
2014-10-21 02:45:22 -0200

    Alteração na pagina de usuarios 2

--- usuarios.php
+++ usuarios.php
@@ -21,94 +21,61 @@
       include("header.php");
     ?>
 
-    <div class="col-xs-4" style="margin: 4% auto auto; float: none; width: 950px;">
+    <div class="col-xs-4" style="margin: 4% auto auto; float: none; width: 950px; min-height: 435px">
       <div class="panel panel-default">
-        <div style="height: 35px">
+        <div style="height: 35px;">
           <table class="table table-striped">
             <thead>
               <tr class="info">
-                <td style="width: 9%">
+                <td style="width: 9.5%">
                   ID
                 </td>
-                <td style="width: 35%">
+                <td style="width: 36%">
                   Nome
                 </td>
-                <td style="width: 18%">
+                <td style="width: 13%">
                   Login
                 </td>
-                <td style="width: 26.5%">
+                <td style="width: 30%">
                   Email
                 </td>
-                <td style="width: 14%">
+                <td style="width: 17%">
                   Telefone
                 </td>
               </tr>
             </thead>
           </table>
         </div>
-        <div style="max-height: 400px; overflow: auto; width: 100%">
-        <table class="table table-striped header-fixed">          
+        <div style="height: 400px; overflow-y: scroll; width: 100%">
+        <table class="table table-striped header-fixed" data-single-select="true">
           <body>
-            <tr>
-              <td class="col-xs-1">
-                1
-              </td>
-              <td class="col-xs-4">
-                Thiago
-              </td>
-              <td class="col-xs-2">
-                thiago
-              </td>
-              <td class="col-xs-3">
-                thiago.carvp@gmail.com
-              </td>
-              <td class="col-xs-1">
-              9999-9999
-              </td>
-            </tr>
-
-            <tr>
-              <td>
-                2
-              </td>
-              <td>
-                Renan
-              </td>
-              <td>
-                renan
-              </td>
-              <td>
-                renansd@hotmail.com
-              </td>
-              <td>
-              9999-9999
-              </td>
-            </tr>
-
             <?php
-              $i = 0;
-              while($i < 20)
+              $conexao = mysql_connect("localhost", "root", "123456");
+              mysql_select_db("hidrosys", $conexao);
+
+              $query = "SELECT * FROM usuarios";
+              $result = mysql_query($query);
+              while($consulta = mysql_fetch_array($result))
               {
-                echo '
-                  <tr>
-              <td>
-                2
-              </td>
-              <td>
-                Renan
-              </td>
-              <td>
-                renan
-              </td>
-              <td>
-                renansd@hotmail.com
-              </td>
-              <td>
-              9999-9999
-              </td>
-            </tr>
-                ';
-                $i += 1;
+                echo
+                '<tr>
+                  <td
+                  <td style="width: 10%">
+                    '.$consulta["id"].
+                  '</td>
+                  <td style="width: 40%">
+                    '.$consulta["nome"].
+                  '</td>
+                  <td style="width: 14%">
+                    '.$consulta["login"].
+                  '</td>
+                  <td style="width: 32%">
+                    '.$consulta["email"].
+                  '</td>
+                  <td style="width: 14%">
+                    '.$consulta["telefone"].
+                  '</td>
+                </tr>';
               }
             ?>
           </tbody>
