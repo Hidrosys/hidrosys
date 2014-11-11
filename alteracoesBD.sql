fbc607cf3cc1482fbd983f0f7b631b4cad0cd6a4
thiagocarvp <thiago.carvp@gmail.com>
2014-11-11 15:37:17 -0200

    Implementação mensagem de erro de login; correção de erros na classe usuário, cliente; inserção da tabela de funcionários

---
+++ alteracoesBD.sql
@@ -0,0 +1,60 @@
+-- Alteração em 20/10/2014
+
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` text NOT NULL,
+  `login` varchar(30) NOT NULL,
+  `senha` text NOT NULL,
+  `email` text NOT NULL,
+  `telefone` text NOT NULL,
+  `tipo` int(11) NOT NULL,
+  `deleted` int(11) DEFAULT NULL,
+  `deleted_date` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `login` (`login`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+INSERT INTO `usuarios` (`id`, `nome`, `login`, `senha`, `email`, `telefone`, `tipo`) VALUES
+(1, 'admin', 'admin', 'YWRtaW4=', '', '', 1);
+
+----------------------------------------------------------
+
+
+-- Alteração em 22/10/2014
+
+CREATE TABLE IF NOT EXISTS `clientes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` text NOT NULL,
+  `endereco` text NOT NULL,
+  `cep` text NOT NULL,
+  `telefone` text NOT NULL,
+  `cpf` text NOT NULL,
+  `email` text NOT NULL,
+  `deleted` int(11) DEFAULT NULL,
+  `deleted_date` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+----------------------------------------------------------
+
+-- Alteração em 11/11/2014
+
+CREATE TABLE IF NOT EXISTS `funcionarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `datanasc` text NOT NULL,
+  `rg` text NOT NULL,
+  `cpf` text NOT NULL,
+  `endereco` text NOT NULL,
+  `email` text NOT NULL,
+  `ocupacao` text NOT NULL,
+  `salario` int(11) NOT NULL,
+  `prehora` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `telefone` text NOT NULL,
+  `nome` text NOT NULL,
+  `deleted` int(11) DEFAULT NULL,
+  `deleted_date` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+----------------------------------------------------------
