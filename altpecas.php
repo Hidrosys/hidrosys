7c64ac32bc44443f79fa5c591dbdc4d7d29628f9
renansd <renansouzadaniel@yahoo.com.br>
2014-11-17 20:35:35 -0200

    Atualização - CRUD Peças
    CRUD Peças

---
+++ altpecas.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hidrosys</title>
+
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+	<form method="POST" action="altpecas.php" name="vai" id="vai">
+		<input type="hidden" name="opt" value="1">
+	</form>
+	<?php		
+	if($_POST["opt"]=="1") echo '<div class="panel panel-default">
+		<div class="panel-heading">
+			<h3 class="panel-title">Inserir Ferramentas</h3>
+		</div>
+		<div class="panel-body">
+			<form role="form">
+				<input type="hidden" id="opt" name=opt>
+			  <div class="form-group">
+				<label>Tipo</label>
+				<input class="form-control" id="tipo" placeholder="Entre com o tipo">
+			  </div>
+			  <div class="form-group">
+				<label>Descricao</label>
+				<input type="form-control" class="form-control" id="descricao" placeholder="Entre com uma descricao">
+			  </div>
+			  <div class="form-group">
+				<label>Quantidade</label>
+				<input class="form-control" id="qtd" placeholder="Entre com a quantidade inicial">
+			  </div>
+			  <div class="form-group">
+				<label>Fabricante</label>
+				<input class="form-control" id="fabricante" placeholder="Entre com o fabricante">
+			  </div>
+			  <div class="form-group">
+				<label>Preco Unitário</label>
+				<input class="form-control" id="precoun" placeholder="Entre com o preco unitario">
+			  </div>			  
+			</form>
+			<button onclick="manda()" id="id1" class="btn btn-default">Inserir</button>
+			<button onclick="volta()" id="id2" class="btn btn-default">Voltar</button>
+		</div>
+	</div>';
+	if($_POST["opt"] == '2')
+	{	
+	$conexao = mysqli_connect("localhost", "root", "123456", "hidrosys");
+	$query = "SELECT * FROM pecas WHERE id = ".$_POST["selected_row"];
+	$result = mysqli_query($conexao, $query);
+	$consulta = mysqli_fetch_array($result);	
+	echo '<div class="panel panel-default">
+		<div class="panel-heading">
+			<h3 class="panel-title">Visualizar Cliente</h3>
+		</div>
+		<div class="panel-body">
+			<form role="form">
+			  <div class="form-group">
+				<label>ID</label>
+				'.$_POST["selected_row"].'
+			  </div>
+			  <div class="form-group">
+				<label>Tipo</label>
+				'.$consulta["tipo"].'
+			  </div>
+			  <div class="form-group">
+				<label>Descricao</label>
+				'.$consulta["descricao"].'
+			  </div>
+			  <div class="form-group">
+				<label>Quantidade</label>
+				'.$consulta["quantidade"].'
+			  </div>
+			  <div class="form-group">
+				<label>Fabricante</label>
+				'.$consulta["fabricante"].'
+			  </div>
+			  <div class="form-group">
+				<label>Preco Unitário</label>
+				'.$consulta["precoun"].'
+			  </div>
+			  			  
+			</form>
+			<button onclick="volta()" id="id2" class="btn btn-default">Voltar</button>
+		</div>
+	</div>';	
+	mysqli_close($conexao);
+	}
+
+	if($_POST["opt"]=="3")
+	{		
+		unset($_POST["opt"]);
+		include("classepeca.php");
+		$_POST["opt"]="3";
+		$usuario = consultaBD($_POST["selected_row"]);
+		echo '<div class="panel panel-default">
+		<div class="panel-heading">
+			<h3 class="panel-title">Alterar Ferramenta</h3>
+		</div>
+		<div class="panel-body">
+			<form role="form">
+				<input type="hidden" id="opt" name=opt>
+				<input type="hidden" id="id" value="'. $_POST["selected_row"] .'">
+			  <div class="form-group">
+				<label>Quantidade</label>
+				<input class="form-control" id="qtd" placeholder="Entre com a quantiidade" value="'. $usuario->getQuantidade() .'">
+			  </div>
+			  <div class="form-group">
+				<label>Preco Unitário</label>
+				<input class="form-control" id="precoun" placeholder="Entre com o preco unitario" value="'. $usuario->getPrecoun() .'">
+			  </div>
+			</form>
+			<button onclick="salva()" id="id1" class="btn btn-default">Salvar</button>
+			<button onclick="volta()" id="id2" class="btn btn-default">Voltar</button>
+		</div>
+		</div>';
+	}
+
+	?>
+	<script type="text/javascript">
+	//var op= $_POST["opt"];
+	function manda(){
+	$.ajax({
+	  type: "POST",
+	  url: "classepeca.php",
+	  data: {opt: "1", tipo: document.getElementById("tipo").value, descricao: document.getElementById("descricao").value, qtd: document.getElementById("qtd").value, fabricante: document.getElementById("fabricante").value, precoun: document.getElementById("precoun").value}
+	}).done(function() {		
+		document.vai.submit();	
+	  });		
+	}
+	function volta()
+	{
+		window.location.replace("/hidrosys/pecas.php");
+	}
+	function salva()
+	{
+	$.ajax({
+	  type: "POST",
+	  url: "classepeca.php",
+	  data: {opt: "3", id: document.getElementById("id").value, qtd: document.getElementById("qtd").value, precoun: document.getElementById("precoun").value}
+	}).done(function() {		
+		window.location.replace("/hidrosys/pecas.php");
+	  });
+	}
+	</script>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ classepeca.php
@@ -0,0 +1,142 @@
+<?php
+	include("seguranca.php");
+	class Peca
+	{
+		private $tipo;
+		private $descricao;
+		private $qtd;
+		private $fabricante;	
+		private $precoun;
+		private $id;		
+		
+		public function __construct($tipo, $descricao, $qtd, $fabricante, $precoun, $id)
+		{
+			$this->tipo = $tipo;			
+			$this->descricao = $descricao;
+			$this->qtd = $qtd;
+			$this->fabricante = $fabricante;
+			$this->precoun = $precoun;
+			$this->id = $id;	
+		}
+		
+		public function bdupdate()
+		{
+			$conexao = mysqli_connect("localhost", "root", "123456", "hidrosys");						
+			$query = "UPDATE pecas SET tipo = '" . $this->tipo . "', descricao = '" . $this->descricao . "', quantidade = " . $this->qtd . ", fabricante = '" . $this->fabricante . "', precoun = " . $this->precoun . " WHERE id = " . $this->id;
+			$result = mysqli_query($conexao, $query);
+			mysqli_close($conexao);
+		}
+		public function bdcreate()
+		{
+			$conexao = mysqli_connect("localhost", "root", "123456", "hidrosys");						
+			$query = "INSERT INTO pecas (tipo, descricao, quantidade, fabricante, precoun) VALUES('".$this->tipo . "','" . $this->descricao . "'," . $this->qtd . ",'" . $this->fabricante . "'," . $this->precoun . ")";
+			$result = mysqli_query($conexao, $query);
+			mysqli_close($conexao);
+		}
+		public function getTipo()
+		{
+			return $this->tipo;
+		}
+		public function getDescricao()
+		{
+			return $this->descricao;
+		}
+		public function getQuantidade()
+		{
+			return $this->qtd;
+		} //Necessário?
+		public function getFabricante()
+		{
+			return $this->fabricante;
+		}		
+		public function getID()
+		{
+			return $this->id;
+		}
+		public function getPrecoun()
+		{
+			return $this->precoun;
+		}
+		public function modQuantidade($quantidade)
+		{
+			$this->quantidade = $quantidade;
+		}
+	public function modPrecoun($precoun)
+		{
+			$this->precoun = $precoun;
+		}		
+	}
+	function consultaBD($id_entra)
+	{
+		$conexao = mysqli_connect("localhost", "root", "123456", "hidrosys");
+		$query = "SELECT * FROM pecas WHERE id = ".$id_entra;
+		$result = mysqli_query($conexao, $query);
+		$consulta = mysqli_fetch_array($result);
+		$insta = new Peca($consulta["tipo"], $consulta["descricao"], $consulta["quantidade"], $consulta["fabricante"], $consulta["precoun"], $consulta["id"]);
+		mysqli_close($conexao);
+		return $insta;
+	}
+	function deletaBD($id)
+	{
+		$conexao = mysqli_connect("localhost", "root", "123456", "hidrosys");
+		$query = "UPDATE pecas SET deleted = 1, deleted_date = '" . date("m/d/y G.i:s<br>", time()) . "' WHERE id = ". $id;
+		$result = mysqli_query($conexao, $query);
+		mysqli_close($conexao);		
+	}	
+	function criaBD($tipo, $descricao, $qtd, $fabricante, $precoun)
+	{
+		$novo = new Peca($tipo, $descricao, $qtd, $fabricante, $precoun, 0);
+		$novo->bdcreate();		
+	}
+	function editaBD($tipo, $descricao, $qtd, $fabricante, $precoun, $id)
+	{
+		$edita = new Peca($tipo, $descricao, $qtd, $fabricante, $precoun, $id);
+		$edita->bdupdate();
+	}
+	if(isset($_POST["opt"]))
+	{
+		if($_POST["opt"]=="1")
+		{
+			criaBD($_POST["tipo"], $_POST["descricao"], $_POST["qtd"], $_POST["fabricante"], $_POST["precoun"]);
+			date_default_timezone_set("America/Brasilia");			
+			$formated_date  = date("m/d/y G.i:s<br>", time());	
+			$arquivo = fopen('logpecas.txt','a');
+			fwrite($arquivo, $formated_date . "--Insertion of " . $_POST["tipo"] . " by " . getLogin() . "!\r\n");
+			fclose($arquivo);
+			header("Location: pecas.php");
+		}
+		if($_POST["opt"]=="3")
+		{
+			$conexao = mysqli_connect("localhost", "root", "123456", "hidrosys");
+			$query = "SELECT * FROM pecas WHERE id = ". $_POST["id"];
+			$result = mysqli_query($conexao, $query);
+			$consulta = mysqli_fetch_array($result);			
+			editaBD($consulta["tipo"], $consulta["descricao"], $_POST["qtd"], $consulta["fabricante"], $_POST["precoun"], $consulta["id"]);			
+			//criaBD($_POST["login"], $_POST["senha"], $_POST["nome"], $_POST["email"], $_POST["telefone"], $_POST["tipo"]);
+			mysqli_close($conexao);
+			$conexao = mysqli_connect("localhost", "root", "123456", "hidrosys");
+			$query = "SELECT * FROM pecas WHERE id = ". $_POST["id"];
+			$result = mysqli_query($conexao, $query);
+			$consulta = mysqli_fetch_array($result);
+			$formated_date  = date("m/d/y G.i:s<br>", time());	
+			$arquivo = fopen('logpecas.txt','a');
+			fwrite($arquivo, $formated_date . "--Alteration of " . $consulta["tipo"] . " by " . getLogin() . "!\r\n");
+			fclose($arquivo);
+			mysqli_close($conexao);	
+			header("Location: pecas.php");
+		}
+		if($_POST["opt"]=="4")
+		{
+			$conexao = mysqli_connect("localhost", "root", "123456", "hidrosys");
+			$query = "SELECT * FROM pecas WHERE id = ". $_POST["id"];
+			$result = mysqli_query($conexao, $query);
+			$consulta = mysqli_fetch_array($result);
+			$formated_date  = date("m/d/y G.i:s<br>", time());	
+			$arquivo = fopen('logpecas.txt','a');
+			fwrite($arquivo, $formated_date . "--Deletion of " . $consulta["tipo"] . " by " . getLogin() . "!\r\n");
+			fclose($arquivo);
+			mysqli_close($conexao);		
+			deletaBD($_POST["id"]);			
+		}
+	}	
+?>
